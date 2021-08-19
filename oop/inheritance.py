795d9894751de5517e9ca1518db152db0b54184d
Wanderson Fontes <41520210+WandersonFontes@users.noreply.github.com>
2021-08-18 22:50:53 -0300

    Understanding about inheritance.

---
+++ oop/inheritance.py
@@ -0,0 +1,72 @@
+#Game Player
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+class Magician:
+    def __init__(self, person):
+        self.person = person
+        Person(self.person)
+        print(f'{self.person} is a person of type magician!')
+    def create(self):
+        print(f"{self.person} is a person of type magician!")
+    def attack(self):
+        print('Attack: -100')
+
+class Archer:
+    def __init__(self, person):
+        self.person = person
+        Person(self.person)
+        print(f'{self.person} now you have a archer!')
+    def create(self):
+        print(f"{self.person} is a person of type magician!")
+    def attack(self):
+        print('Attack: -80')
+
+class Ogres:
+    def __init__(self, person):
+        self.person = person
+        Person(self.person)
+        print(f'{self.person} a ogre in war!')
+    def create(self):
+        print(f"{self.person} is a person of type ogre!")
+    def attack(self):
+        print('Attack: -120')
+
+#newPlayer1 = Magician('Wan')
+#newPlayer2 = Archer('Aislan')
+#newPlayer3 = Ogres('Toka')
+
+
+#______________________________________________________________________________________________________________________________________________________________________
+#HousePattern
+
+class House:
+    def __init__(self):
+        print('Welcome your new house!')
+    def clear(self):
+        print('Cleared sucessfully!')
+    def save(self):
+        print('Saved sucessfully!')
+    def toWash(self):
+        print('Washed out sucessfully!')
+
+class Bedroom(House):
+    def toSlep(self):
+        print('Sleping...')
+    def getDressed(self):
+        print('Dressing up...')
+
+
+myBedroom = Bedroom()
+myBedroom.clear()
+myBedroom.toWash()
+myBedroom.toSlep()
+
+
+
+
+
+
+
+
