5341a30ba5ab48214f1ddb31d5b79c7cf6e0951c
Wanderson Fontes <41520210+WandersonFontes@users.noreply.github.com>
2021-09-04 20:49:13 -0300

    Continuation of the course by learning advanced techniques

--- functions/functions.py
+++ functions/functions.py
@@ -28,6 +28,8 @@ def sub(num1, num2):
 # input()
 
 # Functions
+
+
 def new_function():
     print("I'm one function")
 # new_function()
@@ -46,6 +48,8 @@ myCreator()
 # help(myCreator)
 # print(myCreator.__doc__)
 
+# Variable Arguments
+
 
 def super_function_arg(*args):
     print(args)
@@ -57,7 +61,8 @@ def super_function_kwargs(**kwargs):
         print(itens)
     print(kwargs)
 
-
+super_function_arg('a', 'b', 'c')
+super_function_arg(*[1,2,3])
 super_function_kwargs(test1='TEST1', test2='TEST2', test3='TEST3')
 
 # Functions Transformers
@@ -69,14 +74,15 @@ def getNumberOdd(numbres):
         return False
     return True
 
+
 def higherNumber(numbers):
     if numbers == 1:
         return False
     return True
 
+
 def pontecy(numbers):
     return numbers**2
-
 
 
 odd = list(filter(getNumberOdd, num))
@@ -87,3 +93,57 @@ print(higher)
 
 potentiation = list(map(pontecy, num))
 print(potentiation)
+
+
+# ways to interact about arrays
+import itertools
+
+arrayName1 = ['W','A','N','D','E','R','S','O','N']
+arrayName2 = [' ','F','O','N','T','E','S']
+arrayNum = [1,2,3,4,5,6,7,8,9]
+
+# Circle inside array, when finished, restarts from first position
+circle = itertools.cycle(arrayName1)
+print(next(circle),next(circle),next(circle))
+
+# Count
+countNumbers = itertools.count(1, 2)
+print(next(countNumbers), next(countNumbers), next(countNumbers), next(countNumbers))
+
+# Acumulate
+result = itertools.accumulate(arrayNum)
+print(list(result))
+
+# Chain
+myName = itertools.chain(arrayName1, arrayName2)
+print(list(myName))
+
+# Complementary functions
+
+
+def condition(num):
+    return num == 5
+
+# DropWhile
+
+
+print(list(itertools.dropwhile(condition, arrayNum)))
+print(list(itertools.takewhile(condition, arrayNum)))
+
+# Lambda
+# lambda(paramtrs):(expression)
+
+
+def double(num):
+    return num*2
+
+def toLower(arg):
+    return arg.lower()
+
+
+print(list(map(double, arrayNum)))
+print(list(map(toLower, arrayName1)))
+# lambda
+print('-'*50+'\nLambda')
+print(list(map(lambda num: num*2, arrayNum)))
+print(list(map(lambda arg: arg.lower(), arrayName1)))
